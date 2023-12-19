775561b571d31eac699fc5e2107836338c61fe55
Robert Mitwicki <robert@thclab.online>
2023-12-20 00:23:38 +0100

    feat: implement new data structure

--- presentation-base/src/lib.rs
+++ presentation-base/src/lib.rs
@@ -5,18 +5,53 @@ use said::sad::{SerializationFormats, SAD};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, SAD, Serialize, Deserialize)]
-pub struct PresentationBase {
+pub struct Presentation {
     #[serde(rename = "bd")]
     pub bundle_digest: said::SelfAddressingIdentifier,
     #[said]
     #[serde(rename = "d")]
     pub said: Option<said::SelfAddressingIdentifier>,
-    #[serde(rename = "o")]
+    #[serde(rename = "p")]
     pub pages: BTreeMap<String, Vec<String>>,
     #[serde(rename = "po")]
     pub pages_order: Vec<String>,
     #[serde(rename = "pl")]
     pub pages_label: BTreeMap<Language, BTreeMap<String, String>>,
+    #[serde(rename = "i")]
+    pub interfaces: Vec<Interface>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Interface {
+    #[serde(rename = "m")]
+    pub mode: ModeType,
+    #[serde(rename = "c")]
+    purpose: Purpose,
+    #[serde(rename = "a")]
+    attr_properties: BTreeMap<String, Properties>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Properties {
+    #[serde(rename = "t")]
+    pub type_: AttrType,
+}
+
+#[serde(rename_all = "lowercase")]
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub enum AttrType {
+    TextArea
+}
+
+#[serde(rename_all = "lowercase")]
+#[derive(Debug, Serialize, Deserialize, Clone)]
+enum Purpose {
+    Capture,
+}
+#[serde(rename_all = "lowercase")]
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub enum ModeType {
+    Web,
 }
 
 #[cfg(test)]
@@ -38,7 +73,7 @@ mod tests {
         pages_label_en.insert("pageZ".to_string(), "Page Z".to_string());
         pages_label.insert(Language::Eng, pages_label_en);
 
-        let mut presentation_base = PresentationBase {
+        let mut presentation_base = Presentation {
             bundle_digest: "EHp19U2U1sdOBmPzMmILM3DUI0PQph9tdN3KtmBrvNV7"
                 .parse()
                 .unwrap(),
@@ -46,6 +81,18 @@ mod tests {
             pages,
             pages_order: vec!["pageY".to_string(), "pageZ".to_string()],
             pages_label,
+            interfaces: vec![Interface {
+                mode: ModeType::Web,
+                purpose: Purpose::Capture,
+                attr_properties: vec![(
+                    "attr_1".to_string(),
+                    Properties {
+                        type_: AttrType::TextArea,
+                    },
+                )]
+                .into_iter()
+                .collect(),
+            }]
         };
 
         presentation_base.compute_digest();
@@ -56,7 +103,7 @@ mod tests {
         );
         assert_eq!(
             presentation_base.said.unwrap().to_string(),
-            "EB17nP-YYp1eav0KHu6K90iUVn47sR1_z3_dKtoEBL8Z".to_string()
+            "EBDKqlEFqzmW00Q6PzsqXb0rPTXeCqYcHS1kwhg76LiF".to_string()
         );
     }
 }
